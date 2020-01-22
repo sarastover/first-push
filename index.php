b353f0202bb9c6b5988ea1eec82bc5ddf5b2f82f
Sara <60195910+sarastover@users.noreply.github.com>
2020-01-22 12:48:42 -0800

    Push practice
    Uploaded an example PHP file by dragging it in.

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+echo Hello World
+
+?><?php
+
+echo "Hello World";
+
+
+$example = "Sara";
+
+echo "<p>My name is $example</p>";
+
+$string1 = "<p>This is the first part";
+
+$string2 = " of a sentence.</p>";
+
+echo $string1.$string2;
+
+$myNumber = 45;
+$calculation = $myNumber + 31 / 97 + 4;
+
+echo "The result of the calculation is ".$calculation;
+
+$myBool = true;
+
+echo "<p>This statement is true? ".$myBool."</p>";
+
+$variableName = "name";
+
+echo $$variableName;
+
+?>
